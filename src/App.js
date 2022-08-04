2bbd907b6dfbffce594ceff9b51d6b8e0061fad1
noui61 <noui@metavault.org>
2022-08-04 15:17:09 +0300

    reasons section add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Program from './components/Programs/Program';
+import Reasons from './components/Reasons/Reasons';
 function App() {
   return (
     <div className="App">
          <Hero/>
          <Program/>
+         <Reasons/>
     </div>
   );
 }
