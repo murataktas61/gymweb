f1ac98921ad8f91d0371fdb77e389fba61ed0365
noui61 <noui@metavault.org>
2022-08-04 16:57:32 +0300

    testimonials section add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from './components/Hero/Hero';
 import Plans from './components/Plans/Plans';
 import Program from './components/Programs/Program';
 import Reasons from './components/Reasons/Reasons';
+import Testimonials from './components/Testimonials/Testimonials';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
          <Program/>
          <Reasons/>
          <Plans/>
+         <Testimonials/>
     </div>
   );
 }
