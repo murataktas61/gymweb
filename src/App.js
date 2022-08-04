37046da53a6eef41783d314881ba8382803aab4a
noui61 <noui@metavault.org>
2022-08-04 16:09:05 +0300

    plans section add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 import Program from './components/Programs/Program';
 import Reasons from './components/Reasons/Reasons';
 function App() {
@@ -8,6 +9,7 @@ function App() {
          <Hero/>
          <Program/>
          <Reasons/>
+         <Plans/>
     </div>
   );
 }
